48ee4b9506b842a5f9f3da6b0e52fc5a5ece2c8c
Red <red.dev@hexle.at>
2024-11-10 11:38:57 +0100

    feat(friend-system-v3): register commands

--- surf-friends/src/main/java/dev/slne/surf/friends/SurfFriendsPlugin.java
+++ surf-friends/src/main/java/dev/slne/surf/friends/SurfFriendsPlugin.java
@@ -1,11 +1,19 @@
 package dev.slne.surf.friends;
 
+import dev.slne.surf.friends.command.FriendCommand;
+import dev.slne.surf.friends.command.subcommand.FriendAddCommand;
 import dev.slne.surf.friends.core.util.PluginColor;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SurfFriendsPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    this.registerCommands();
+  }
+
   public static Component getPrefix() {
     return Component.text(">> ").color(NamedTextColor.GRAY)
         .append(Component.text("Friends").color(PluginColor.LIGHT_BLUE))
@@ -14,5 +22,10 @@ public class SurfFriendsPlugin extends JavaPlugin {
 
   public static SurfFriendsPlugin getInstance() {
     return getPlugin(SurfFriendsPlugin.class);
+  }
+
+  private void registerCommands() {
+    new FriendCommand("friend").register();
+    new FriendAddCommand("fa").register();
   }
 }
