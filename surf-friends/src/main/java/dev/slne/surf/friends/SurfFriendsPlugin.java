ba75e17baeb53a11e81cba8bd09e38e6cd571071
Red <red.dev@hexle.at>
2024-11-10 12:04:38 +0100

    feat(friend-system-v3): register listener and save all on disable

--- surf-friends/src/main/java/dev/slne/surf/friends/SurfFriendsPlugin.java
+++ surf-friends/src/main/java/dev/slne/surf/friends/SurfFriendsPlugin.java
@@ -3,8 +3,11 @@ package dev.slne.surf.friends;
 import dev.slne.surf.friends.command.FriendCommand;
 import dev.slne.surf.friends.command.subcommand.FriendAddCommand;
 import dev.slne.surf.friends.core.util.PluginColor;
+import dev.slne.surf.friends.listener.PlayerJoinListener;
+import dev.slne.surf.friends.listener.PlayerQuitListener;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SurfFriendsPlugin extends JavaPlugin {
@@ -12,6 +15,11 @@ public class SurfFriendsPlugin extends JavaPlugin {
   @Override
   public void onEnable() {
     this.registerCommands();
+  }
+
+  @Override
+  public void onDisable() {
+    FriendManager.instance().saveAll();
   }
 
   public static Component getPrefix() {
@@ -27,5 +35,11 @@ public class SurfFriendsPlugin extends JavaPlugin {
   private void registerCommands() {
     new FriendCommand("friend").register();
     new FriendAddCommand("fa").register();
+  }
+
+  private void registerListener() {
+    Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
+    Bukkit.getPluginManager().registerEvents(new PlayerQuitListener(), this);
+
   }
 }
