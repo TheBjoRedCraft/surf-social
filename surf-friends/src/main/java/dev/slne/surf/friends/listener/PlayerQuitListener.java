8b08eaaca0bcc1e52e538c9d83a3576e07d40a4b
Red <red.dev@hexle.at>
2024-11-10 12:03:55 +0100

    feat(friend-system-v3): create listener & handle quit

---
+++ surf-friends/src/main/java/dev/slne/surf/friends/listener/PlayerQuitListener.java
@@ -0,0 +1,16 @@
+package dev.slne.surf.friends.listener;
+
+import dev.slne.surf.friends.FriendManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener implements Listener {
+  @EventHandler
+  public void onQuit(PlayerQuitEvent event) {
+    Player player = event.getPlayer();
+
+    FriendManager.instance().saveFriendData(player.getUniqueId());
+  }
+}
