9de76b3b8dcae44e9aa674c6aa14991440e5a819
Red <red.dev@hexle.at>
2024-12-27 19:04:33 +0100

    fix(friend-system-v3): back item for closing menu

--- surf-friends/src/main/java/dev/slne/surf/friends/menu/FriendMainMenu.kt
+++ surf-friends/src/main/java/dev/slne/surf/friends/menu/FriendMainMenu.kt
@@ -3,10 +3,12 @@ package dev.slne.surf.friends.menu
 import com.github.shynixn.mccoroutine.bukkit.launch
 import com.github.stefvanschie.inventoryframework.pane.OutlinePane
 import com.github.stefvanschie.inventoryframework.pane.Pane
+import com.github.stefvanschie.inventoryframework.pane.StaticPane
 import dev.slne.surf.friends.FriendManager
 import dev.slne.surf.friends.SurfFriendsPlugin
 import dev.slne.surf.friends.listener.util.ItemBuilder
 import dev.slne.surf.friends.listener.util.PluginColor
+import dev.slne.surf.friends.listener.util.buildGuiItem
 import dev.slne.surf.friends.menu.sub.friend.FriendFriendsMenu
 import dev.slne.surf.friends.menu.sub.request.FriendRequestsMenu
 import net.kyori.adventure.text.Component
@@ -18,72 +20,30 @@ class FriendMainMenu : FriendMenu(5, "Freunde") {
         val header = OutlinePane(0, 0, 9, 1, Pane.Priority.LOW)
         val footer = OutlinePane(0, 4, 9, 1, Pane.Priority.LOW)
 
+        val navigation = StaticPane(0, 4, 9, 1, Pane.Priority.HIGH)
+
         val flPane = OutlinePane(2, 2, 1, 1)
         val frPane = OutlinePane(6, 2, 1, 1)
 
         val friendList = ItemBuilder(Material.ENDER_PEARL)
             .setName(Component.text("Freunde").color(PluginColor.BLUE_LIGHT))
-            .addLoreLine(
-                Component.text("Freundesliste", PluginColor.DARK_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text(
-                    "Hier findest du eine Auflistung aller",
-                    PluginColor.LIGHT_GRAY
-                ).decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text("Freunde von dir.", PluginColor.LIGHT_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text("Du kannst diesen hier nachjoinen", PluginColor.LIGHT_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text("oder sie entfernen.", PluginColor.LIGHT_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .build()
+            .addLoreLine(Component.text("Freundesliste", PluginColor.DARK_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("Hier findest du eine Auflistung aller", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("Freunde von dir.", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("Du kannst diesen hier nachjoinen", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("oder sie entfernen.", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false)).build()
 
         val friendRequests = ItemBuilder(Material.PAPER)
             .setName(Component.text("Freundschaftsanfragen").color(PluginColor.BLUE_LIGHT))
-            .addLoreLine(
-                Component.text("Freundschaftsanfragen", PluginColor.DARK_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text("Hier findest du eine Auflistung", PluginColor.LIGHT_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text(
-                    "aller offenen Freundschaftsanfragen.",
-                    PluginColor.LIGHT_GRAY
-                ).decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text("Du kannst diese direkt hier", PluginColor.LIGHT_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .addLoreLine(
-                Component.text("annehmen oder akzeptieren.", PluginColor.LIGHT_GRAY)
-                    .decoration(TextDecoration.ITALIC, false)
-            )
-            .build()
+            .addLoreLine(Component.text("Freundschaftsanfragen", PluginColor.DARK_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("Hier findest du eine Auflistung", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("aller offenen Freundschaftsanfragen.", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text(" ", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("Du kannst diese direkt hier", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false))
+            .addLoreLine(Component.text("annehmen oder akzeptieren.", PluginColor.LIGHT_GRAY).decoration(TextDecoration.ITALIC, false)).build()
 
         header.addItem(build(ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("")))
         header.setRepeat(true)
@@ -91,8 +51,7 @@ class FriendMainMenu : FriendMenu(5, "Freunde") {
         footer.addItem(build(ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("")))
         footer.setRepeat(true)
 
-        flPane.addItem(
-            build(friendList) {
+        flPane.addItem(build(friendList) {
                 if (it == null) {
                     return@build
                 }
@@ -103,10 +62,7 @@ class FriendMainMenu : FriendMenu(5, "Freunde") {
             }
         )
 
-        frPane.addItem(
-            build(
-                friendRequests
-            ) {
+        frPane.addItem(build(friendRequests) {
                 if (it == null) {
                     return@build
                 }
@@ -116,7 +72,12 @@ class FriendMainMenu : FriendMenu(5, "Freunde") {
                         it.whoClicked
                     )
                 }
-            })
+            }
+        )
+
+        navigation.addItem(buildGuiItem(backItem) {
+            it.whoClicked.closeInventory()
+        }, 4, 0)
 
 
         addPane(header)
