ade24448b461915d7bb6d19864c786fb9736d783
TheBjoRedCraft <red.dev@hexle.at>
2025-01-07 16:51:21 +0100

    fix(friend-system-v4): util

---
+++ surf-friends-velocity/src/main/kotlin/dev/slne/surf/social/friends/util/PlayerUtil.kt
@@ -0,0 +1,9 @@
+package dev.slne.surf.social.friends.util
+
+import com.velocitypowered.api.proxy.Player
+import dev.slne.surf.social.friends.SurfFriends
+import java.util.UUID
+
+object PlayerUtil {
+
+}
