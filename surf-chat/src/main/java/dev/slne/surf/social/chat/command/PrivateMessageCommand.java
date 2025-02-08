fd66a203d2ea1d5d953f8bb37ccec2d4b33a9a6a
TheBjoRedCraft <red.dev@hexle.at>
2025-02-08 02:32:48 +0100

    feat(surf-chat): pms

---
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/command/PrivateMessageCommand.java
@@ -0,0 +1,26 @@
+package dev.slne.surf.social.chat.command;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import dev.slne.surf.social.chat.provider.ConfigProvider;
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
+
+public class PrivateMessageCommand extends CommandAPICommand {
+  public PrivateMessageCommand(String commandName) {
+    super(commandName);
+
+    withArguments(new PlayerArgument("player"));
+    withArguments(new GreedyStringArgument("message"));
+    withPermission("surf.chat.command.private-message");
+    executesPlayer((player, args) -> {
+      Player target = args.getUnchecked("player");
+      String message = args.getUnchecked("message");
+
+      target.sendMessage(MiniMessage.miniMessage().deserialize(PlaceholderAPI.setPlaceholders(player, ConfigProvider.getInstance().getPRIVATE_MESSAGE_FORMAT()).replace("%message%", message)));
+      player.sendMessage(MiniMessage.miniMessage().deserialize(PlaceholderAPI.setPlaceholders(target, ConfigProvider.getInstance().getPRIVATE_MESSAGE_FORMAT()).replace("%message%", message)));
+    });
+  }
+}
