9b2587b31dc2e597ac1129ba9cf504200b684d5e
TheBjoRedCraft <red.dev@hexle.at>
2025-02-08 09:40:50 +0100

    feat(surf-chat): add message to ch and correct replace placeholder

--- surf-chat/src/main/java/dev/slne/surf/social/chat/listener/PlayerAsyncChatListener.java
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/listener/PlayerAsyncChatListener.java
@@ -1,9 +1,14 @@
 package dev.slne.surf.social.chat.listener;
 
+import dev.slne.surf.social.chat.object.Message;
 import dev.slne.surf.social.chat.provider.ConfigProvider;
+import dev.slne.surf.social.chat.service.ChatHistoryService;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,6 +22,16 @@ public class PlayerAsyncChatListener implements Listener {
       return;
     }
 
-    event.message(MiniMessage.miniMessage().deserialize(PlaceholderAPI.setPlaceholders(player, ConfigProvider.getInstance().getPublicMessageFormat()).replace("%player%", player.getName())));
+    Component message = MiniMessage.miniMessage().deserialize(PlaceholderAPI.setPlaceholders(player, ConfigProvider.getInstance().getPublicMessageFormat())).replaceText(TextReplacementConfig.builder()
+        .matchLiteral("%message%")
+        .replacement(event.message())
+        .build());
+    Bukkit.getOnlinePlayers().forEach(online -> ChatHistoryService.getInstance().insertNewMessage(online.getUniqueId(), Message.builder()
+        .receiver(online.getName())
+        .sender(player.getName())
+        .message(message)
+        .build()));
+
+    event.message(message);
   }
 }
