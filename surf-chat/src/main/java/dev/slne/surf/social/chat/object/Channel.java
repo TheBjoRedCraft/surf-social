b3b3bb666f64337f6fbd06a7c73f67f5b7685e01
TheBjoRedCraft <red.dev@hexle.at>
2025-02-08 02:38:00 +0100

    feat(surf-chat): started on channels

---
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/object/Channel.java
@@ -0,0 +1,16 @@
+package dev.slne.surf.social.chat.object;
+
+import it.unimi.dsi.fastutil.objects.ObjectSet;
+import org.bukkit.OfflinePlayer;
+
+public class Channel {
+  private OfflinePlayer owner;
+  private ObjectSet<OfflinePlayer> members;
+  private ObjectSet<OfflinePlayer> invites;
+
+  private String name;
+  private String description;
+
+  private boolean closed;
+
+}
---
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/provider/ChannelProvider.java
@@ -0,0 +1,10 @@
+package dev.slne.surf.social.chat.provider;
+
+import lombok.Getter;
+
+public class ChannelProvider {
+  @Getter
+  private static final ChannelProvider instance = new ChannelProvider();
+
+
+}
