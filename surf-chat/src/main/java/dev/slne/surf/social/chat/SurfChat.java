63ebbfc2606ad9fc59ef683f9fa8b76ead1e918a
TheBjoRedCraft <red.dev@hexle.at>
2025-02-08 10:05:16 +0100

    feat(surf-chat): register listener

--- surf-chat/src/main/java/dev/slne/surf/social/chat/SurfChat.java
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/SurfChat.java
@@ -1,7 +1,11 @@
 package dev.slne.surf.social.chat;
 
 import dev.slne.surf.social.chat.command.PrivateMessageCommand;
+import dev.slne.surf.social.chat.command.SurfChatCommand;
+import dev.slne.surf.social.chat.listener.PlayerAsyncChatListener;
+import dev.slne.surf.social.chat.listener.PlayerQuitListener;
 import dev.slne.surf.social.chat.service.ChatFilterService;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SurfChat extends JavaPlugin {
@@ -13,6 +17,9 @@ public class SurfChat extends JavaPlugin {
   @Override
   public void onEnable() {
     new PrivateMessageCommand("msg").register();
+
+    Bukkit.getPluginManager().registerEvents(new PlayerAsyncChatListener(), this);
+    Bukkit.getPluginManager().registerEvents(new PlayerQuitListener(), this);
   }
 
   public static SurfChat getInstance() {
