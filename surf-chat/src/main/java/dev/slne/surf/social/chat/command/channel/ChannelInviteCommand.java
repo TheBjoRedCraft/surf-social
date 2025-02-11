176c4710322c1326e34cd8b2b4c0df046b6c6a94
TheBjoRedCraft <red.dev@hexle.at>
2025-02-11 08:48:08 +0100

    fix(surf-chat): fix that only moderators can execute mod actions

--- surf-chat/src/main/java/dev/slne/surf/social/chat/command/channel/ChannelInviteCommand.java
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/command/channel/ChannelInviteCommand.java
@@ -21,7 +21,7 @@ public class ChannelInviteCommand extends CommandAPICommand {
         return;
       }
 
-      if(!channel.isModerator(player)) {
+      if(!channel.isModerator(player) && !channel.isOwner(player)) {
         return;
       }
 
