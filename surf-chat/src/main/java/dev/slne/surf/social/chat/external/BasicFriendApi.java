fdb958a6a583ca7e488eaba1ab73867ba08e04d4
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-16 20:01:05 +0100

    feat(surf-chat): add basicfriendapi

---
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/external/BasicFriendApi.java
@@ -0,0 +1,10 @@
+package dev.slne.surf.social.chat.external;
+
+import java.util.UUID;
+
+public class BasicFriendApi {
+  public static boolean areFriends(UUID player, UUID target) {
+    return false; //TODO: Friend API
+  }
+
+}
