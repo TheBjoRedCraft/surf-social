1eac4e301f1706ba4e7a43dbab4ab023dbd93cbb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-17 19:14:59 +0100

    feat(surf-chat): add message

--- surf-chat/src/main/java/dev/slne/surf/social/chat/command/SurfChatSaveCommand.kt
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/command/SurfChatSaveCommand.kt
@@ -7,16 +7,19 @@ import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import dev.slne.surf.social.chat.SurfChat
 import dev.slne.surf.social.chat.`object`.ChatUser
 import dev.slne.surf.social.chat.service.DatabaseService
+import dev.slne.surf.social.chat.util.MessageBuilder
 import org.bukkit.entity.Player
 
 class SurfChatSaveCommand(commandName: String) : CommandAPICommand(commandName) {
     init {
         withPermission("surf.chat.command.save")
-        executesPlayer(PlayerCommandExecutor { _: Player, _: CommandArguments ->
+        executesPlayer(PlayerCommandExecutor { player: Player, _: CommandArguments ->
             SurfChat.instance.launch {
                 ChatUser.cache.synchronous().asMap().values.forEach { user ->
                     DatabaseService.instance.saveUser(user)
                 }
+
+                SurfChat.send(player, MessageBuilder().success("Alle Nutzer wurden gespeichert."))
             }
         })
     }
