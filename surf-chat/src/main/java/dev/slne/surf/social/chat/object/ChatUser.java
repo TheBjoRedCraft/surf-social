8ad2accbdd60fd071a85f81b79033a82021db9fa
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-17 16:26:04 +0100

    feat(surf-chat): save user when removing/invalidating

--- surf-chat/src/main/java/dev/slne/surf/social/chat/object/ChatUser.java
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/object/ChatUser.java
@@ -2,10 +2,14 @@ package dev.slne.surf.social.chat.object;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+
 import dev.slne.surf.social.chat.service.DatabaseService;
 
 import it.unimi.dsi.fastutil.objects.ObjectSet;
+
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +21,8 @@ public class ChatUser {
   @Getter
   private static final LoadingCache<UUID, ChatUser> cache = Caffeine
       .newBuilder()
+      .removalListener((_, user, _) -> DatabaseService.getInstance().saveUser((ChatUser) user))
+      .expireAfterWrite(30, TimeUnit.MINUTES)
       .build(DatabaseService.getInstance()::loadUser);
 
   public static ChatUser getUser(UUID uuid) {
