ee5f5372baf7c001c8c260af46ab1dd10b6a0089
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-17 18:28:42 +0100

    feat(surf-chat): cleanup and fix provider

--- surf-chat/src/main/java/dev/slne/surf/social/chat/provider/ChannelProvider.kt
+++ surf-chat/src/main/java/dev/slne/surf/social/chat/provider/ChannelProvider.kt
@@ -5,19 +5,19 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectMap
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 
 import org.bukkit.entity.Player
+import java.util.*
 
 class ChannelProvider {
-    private val channels: Object2ObjectMap<java.util.UUID, Channel> = Object2ObjectOpenHashMap()
+    val channels: Object2ObjectMap<UUID, Channel> = Object2ObjectOpenHashMap()
 
     fun exists(name: String): Boolean {
         return channels.values.stream().filter { channel: Channel -> channel.name == name }
-            .findFirst().orElse(null) != null
+            .findFirst()
+            .orElse(null) != null
     }
 
     fun handleQuit(player: Player) {
-        val channel: Channel =
-            Channel.Companion.getChannel(player)
-                ?: return
+        val channel: Channel = Channel.getChannel(player) ?: return
 
         if (channel.owner == player) {
             channel.delete()
@@ -25,7 +25,6 @@ class ChannelProvider {
     }
 
     companion object {
-        @Getter
-        private val instance = ChannelProvider()
+        val instance = ChannelProvider()
     }
 }
